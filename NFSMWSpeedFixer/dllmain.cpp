98bdc0901845a508bb2b50bb9e977e82bd80a7a7
rng-guy <dcim.koor@gmail.com>
2026-03-30 12:56:49 +0200

    Reformatted compatibility check.

--- NFSMWSpeedFixer/dllmain.cpp
+++ NFSMWSpeedFixer/dllmain.cpp
@@ -3,14 +3,13 @@
 
 #if !defined(_MSC_VER)
 #error "SpeedFixer requires MSVC."
-#endif
 
-#if !defined(_WIN32) || defined(_WIN64)
+#elif !defined(_WIN32) || defined(_WIN64)
 #error "SpeedFixer requires 32-bit Windows."
-#endif
 
-#if !defined(_MSVC_LANG) || (_MSVC_LANG < 202002L)
+#elif !defined(_MSVC_LANG) || (_MSVC_LANG < 202002L)
 #error "SpeedFixer requires C++20 or newer."
+
 #endif
 
 
